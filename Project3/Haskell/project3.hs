4e321318fc321df2d3a5c20fef9b6c5232300c29
Ben Waters <Ben.Waters@1901group.com>
2015-04-27 13:03:41 -0400

    added data types and getter methods

--- Project3/Haskell/project3.hs
+++ Project3/Haskell/project3.hs
@@ -11,8 +11,10 @@ randomDelay max = do
 	n <- getStdRandom (randomR (1,max))
 	threadDelay (n*100000)
 
+data Card = C ((String, Maybe ThreadId),(String, Maybe ThreadId),(String, Maybe ThreadId), (String, Maybe ThreadId), (String, Maybe ThreadId),(String, Maybe ThreadId),(String, Maybe ThreadId),(String, Maybe ThreadId))
+data Leader = L ThreadId Card
+data Follower = F ThreadId [ThreadId]
 
-data DanceCard = D ((String, Maybe ThreadId),(String, Maybe ThreadId),(String, Maybe ThreadId), (String, Maybe ThreadId), (String, Maybe ThreadId),(Stirng, Maybe ThreadId),(String, Maybe ThreadId),(String, Maybe ThreadId))
 dances = ("Waltz", "Tango", "Foxtrot", "Quickstep", "Rumba", "Samba", "Cha Cha", "Jive")
 emptyDance = "------"
 
@@ -22,8 +24,26 @@ Create threads for each follower
 
 The process of asking should be:
 	1. Check follower thread ID
-	2. if dance card is full done, elsewise do next
-	3. If thread id isn't in dance list, send request
+	2. if dance card is full ::done::, elsewise do next
+	3. If thread id isn't in dance list, send request to random non matched follower
 	4. elsewise go to next follower
--}
+
 dance:: 
+-}
+
+getLeaderID:: Leader -> ThreadId
+getLeaderID L id card = id
+
+getDanceCard:: Leader -> Card
+getDanceCard L id card = card
+
+getFollowerID:: Follower -> ThreadId
+getFollowerID F id idList = id
+
+getLeadersFromFollower:: Follower -> [ThreadId]
+getLeadersFromFollower F id idList = idList
+
+hasDancedWithLeader:: ThreadId -> Follower -> Bool
+hasDancedWithLeader id follower
+	| id `elem` (getLeadersFromFollower follower) = True
+	| otherwise = False
