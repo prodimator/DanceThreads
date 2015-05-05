51b95124c3954d88facb0315b01e249123286497
Ben Waters <bsawyerwaters@gmail.com>
2015-05-05 02:03:01 -0400

    writing logic for concurrent part

--- Project3/Haskell/DancingPhilosophers.hs
+++ Project3/Haskell/DancingPhilosophers.hs
@@ -21,7 +21,7 @@ data FollowerPostOffice = FPO [(MVar Int, MVar Bool)]
 
 danceMapList = [("Waltz", -1), ("Tango", -1), ("Foxtrot", -1), ("Quickstep", -1), ("Rumba", -1), ("Samba", -1), ("Cha Cha", -1), ("Jive", -1)]
 
-danceKeys = ("Waltz", "Tango", "Foxtrot", "Quickstep", "Rumba", "Samba", "Cha Cha", "Jive")
+danceKeys = ["Waltz", "Tango", "Foxtrot", "Quickstep", "Rumba", "Samba", "Cha Cha", "Jive"]
 emptyDance = "------"
 
 createLeader:: Int -> [(String, Integer)] -> Leader
@@ -40,16 +40,42 @@ createFollowers m followers
 	| m > 0 = createFollowers (m-1) (followers++[createFollower m danceMapList])
 	| otherwise = followers
 
-getMap:: Follower -> (Map String Integer)
-getMap (F id map) = map
+getFollowerDanceCard:: Follower -> (Map String Integer)
+getFollowerDanceCard (F id map) = map
 
+updateFollowerDance:: Follower -> Integer -> String -> (Map String Integer)
+updateFollowerDance f id dance = Map.insert dance id (getFollowerDanceCard f)
+
+updateFollowerDanceCard:: Follower -> (Map String Integer) -> Follower
+updateFollowerDanceCard f map = F (getFollowerID f) map
+
+getFollowerID:: Follower -> Int
+getFollowerID (F id map) = id
+
+getLeaderDanceCard:: Leader -> (Map String Integer)
+getLeaderDanceCard (L id map) = map
+
+updateLeaderDance:: Leader -> Integer -> String -> (Map String Integer)
+updateLeaderDance l id dance = Map.insert dance id (getLeaderDanceCard l)
+
+updateLeaderDanceCard:: Leader -> (Map String Integer) -> Leader
+updateLeaderDanceCard l map = L (getLeaderID l) map
+
+getLeaderID:: Leader -> Int
+getLeaderID (L id map) = id
 
 canDance:: Follower -> Integer -> String -> Bool
 canDance f id dance
-	| Maybe.fromJust (Map.lookup dance (getMap f)) == -1 = True
-	| length (filter (==id) (Map.elems (getMap f))) > 0 && length (filter (==id) (Map.elems (getMap f))) < 2 = True
+	| Maybe.fromJust (Map.lookup dance (getFollowerDanceCard f)) == -1 = True
+	| length (filter (==id) (Map.elems (getFollowerDanceCard f))) > 0 && length (filter (==id) (Map.elems (getFollowerDanceCard f))) < 2 = True
 	| otherwise = False
 
+
+getNextDance:: Leader -> [String] -> String
+getNextDance l [] = ""
+getNextDance l (x:xs)
+	| Maybe.fromJust (Map.lookup x (getLeaderDanceCard l)) == -1 = x
+	| otherwise = getNextDance l xs
 
 lookupDanceCard:: String -> (Map String Integer) -> Integer
 lookupDanceCard key map = Maybe.fromJust (Map.lookup key map)
@@ -74,6 +100,41 @@ tellLeaders::[Leader] -> String
 tellLeaders [] = ""
 tellLeaders (x:xs) = tellLeader x ++ "\n\n" ++ tellLeaders xs
 
+{-|
+placeRequestInPostOffice request index postOffice
+placeResponseInPostOffice response index postOffice
+
+retrieveRequestFromPostOffice index postOffice
+retrieveResponseFromPostOffice index postOffice
+
+leader:: Int -> Int -> LeaderPostOffice -> FollowerPostOffice -> Leader -> Int -> IO ()
+leader n m leaderPostOffice followerPostOffice leaderIn counter index = do
+	if leaderDone leaderIn == True
+		then
+			do 
+				return ()
+	else
+		do
+			dance <- getNextDance leaderIn dances
+			placeRequestInPostOffice dance index followerPostOffice
+			leader n m leaderPostOffice followerPostOffice leaderIn (counter + 1) (index + 1 `mod` m)
+
+follower:: Int -> Int -> LeaderPostOffice -> FollowerPostOffice -> Follower -> Int -> IO ()
+follower n m leaderPostOffice followerPostOffice followerIn counter
+	if followerDone followerIn == True
+		then
+			do
+				return ()
+	else
+		do
+			request <- retrieveRequestFromPostOffice (getID followerIn) followerPostOffice
+			if canDance request
+				then
+					do
+						placeResponseInPostOffice
+						updateDanceCard 
+-}
+
 
 main::IO ()
 main = do
@@ -82,6 +143,6 @@ main = do
 	let m = read (argsList!!1)::Int
 	let leaders = createLeaders n []
 	let followers = createFollowers m []
-	blankLeaders <- replicateM n $ newMVar ()
-	blankFollowers <- replicateM m $ newMVar ()
+	blankLeaders <- replicateM n $ newEmptyMVar
+	blankFollowers <- replicateM m $ newEmptyMVar
 	putStrLn $ tellLeaders (reverse leaders)
